747c4e6f1b007d264303c023628e35d741d2b5b6
WellanSok <68143514+WellanSok@users.noreply.github.com>
2023-05-08 20:24:41 -0400

    copied over

---
+++ classification.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+"""
+This is the script used to train an activity recognition 
+classifier on accelerometer data.
+
+"""
+
+import os
+import sys
+import numpy as np
+import sklearn
+import sklearn.metrics
+from sklearn.tree import export_graphviz
+from sklearn.model_selection import train_test_split
+from sklearn import model_selection
+from features import extract_features
+from util import slidingWindow, reorient, reset_vars
+from sklearn.tree import DecisionTreeClassifier
+import pickle
+
+import labels
+
+
+# %%---------------------------------------------------------------------------
+#
+#		                 Load Data From Disk
+#
+# -----------------------------------------------------------------------------
+
+print("Loading data...")
+sys.stdout.flush()
+data_file = 'data/all_labeled_data.csv'
+data = np.genfromtxt(data_file, delimiter=',')
+print("Loaded {} raw labelled activity data samples.".format(len(data)))
+sys.stdout.flush()
+
+# %%---------------------------------------------------------------------------
+#
+#		                    Pre-processing
+#
+# -----------------------------------------------------------------------------
+
+print("Reorienting accelerometer data...")
+sys.stdout.flush()
+reset_vars()
+reoriented = np.asarray([reorient(data[i,2], data[i,3], data[i,4]) for i in range(len(data))])
+reoriented_data_with_timestamps = np.append(data[:,0:2],reoriented,axis=1)
+data = np.append(reoriented_data_with_timestamps, data[:,-1:], axis=1)
+
+data = np.nan_to_num(data)
+
+# %%---------------------------------------------------------------------------
+#
+#		                Extract Features & Labels
+#
+# -----------------------------------------------------------------------------
+
+window_size = 20
+step_size = 20
+
+# sampling rate should be about 100 Hz (sensor logger app); you can take a brief window to confirm this
+n_samples = 1000
+time_elapsed_seconds = (data[n_samples,1] - data[0,1])
+sampling_rate = n_samples / time_elapsed_seconds
+
+print("Sampling Rate: " + str(sampling_rate))
+
+# TODO: list the class labels that you collected data for in the order of label_index (defined in labels.py)
+class_names = labels.activity_labels
+
+print("Extracting features and labels for window size {} and step size {}...".format(window_size, step_size))
+sys.stdout.flush()
+
+X = []
+Y = []
+feature_names = []
+for i,window_with_timestamp_and_label in slidingWindow(data, window_size, step_size):
+    window = window_with_timestamp_and_label[:,2:-1]
+    # print("window = ")
+    # print(window)
+    feature_names, x = extract_features(window)
+    X.append(x)
+    Y.append(window_with_timestamp_and_label[10, -1])
+    
+X = np.asarray(X)
+Y = np.asarray(Y)
+n_features = len(X)
+    
+print("Finished feature extraction over {} windows".format(len(X)))
+print("Unique labels found: {}".format(set(Y)))
+print("\n")
+sys.stdout.flush()
+
+# %%---------------------------------------------------------------------------
+#
+#		                Train & Evaluate Classifier
+#
+# -----------------------------------------------------------------------------
+
+
+# TODO: split data into train and test datasets using 10-fold cross validation
+cv = model_selection.KFold(n_splits=10, random_state=None, shuffle=True)
+
+"""
+TODO: iterating over each fold, fit a decision tree classifier on the training set.
+Then predict the class labels for the test set and compute the confusion matrix
+using predicted labels and ground truth values. Print the accuracy, precision and recall
+for each fold.
+"""
+accuracy_list = []
+precision_list = []
+recall_list = []
+for train_index, test_index in cv.split(X):
+    #do the stuff this loop iterates over the folds
+    betterTree = DecisionTreeClassifier(criterion="entropy", max_depth=4)
+    betterTree.fit(X[train_index], Y[train_index])
+    y_test = Y[test_index]
+    y_train = betterTree.predict(X[test_index])
+    print(sklearn.metrics.confusion_matrix(y_test, y_train))
+    precision_list.append(sklearn.metrics.precision_score(y_test,y_train,average=None))
+    accuracy_list.append(sklearn.metrics.accuracy_score(y_test,y_train))
+    recall_list.append(sklearn.metrics.recall_score(y_test,y_train, average=None))
+# TODO: calculate and print the average accuracy, precision and recall values over all 10 folds
+print(accuracy_list)
+print(f"Accuracy:{np.average(accuracy_list)}")
+print(f"Precision:{np.average(precision_list)}")
+print(f"Recall:{np.average(recall_list)}")
+
+# TODO: train the decision tree classifier on entire dataset
+superTree = DecisionTreeClassifier(criterion="entropy", max_depth=4)
+superTree.fit(X, Y)
+
+# TODO: Save the decision tree visualization to disk - replace 'tree' with your decision tree and run the below line
+export_graphviz(superTree, out_file='tree.dot', feature_names = feature_names)
+
+# TODO: Save the classifier to disk - replace 'tree' with your decision tree and run the below line
+print("saving classifier model...")
+with open('classifier.pickle', 'wb') as f:
+     pickle.dump(superTree, f)
---
+++ features.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+"""
+This file is used for extracting features over windows of tri-axial accelerometer 
+data. We recommend using helper functions like _compute_mean_features(window) to 
+extract individual features.
+
+As a side note, the underscore at the beginning of a function is a Python 
+convention indicating that the function has private access (although in reality 
+it is still publicly accessible).
+
+"""
+
+import numpy as np
+from scipy import signal 
+from scipy.signal import find_peaks
+
+def _compute_mean_features(window):
+    """
+    Computes the mean x, y and z acceleration over the given window. 
+    """
+    return np.mean(window, axis=0)
+
+# TODO: define functions to compute more features
+
+def compute_std(window):
+    """
+    Computes the std over given window i used the comp mean function as template
+    """
+    std_w = np.std(window)
+    return std_w
+
+def rms(window):
+    rms_w = np.sqrt(np.mean(np.square(window)))
+    return rms_w
+
+def domFreq(window):
+    magnitude = np.linalg.norm(window, axis=1)
+    # Compute the DFT of the magnitude signal
+    dft = np.fft.rfft(magnitude).astype(float)
+    # Compute the power spectrum of the DFT
+    power_spectrum = np.square(np.abs(dft))
+
+    # Find the dominant frequency of the signal
+    dominant_freq_bin = np.argmax(power_spectrum)
+    dominant_freq = np.fft.rfftfreq(len(magnitude))[dominant_freq_bin]
+
+    return dominant_freq
+
+# returns length 4 array of x y z and magnitude entropy 
+def entropy(window):
+    entropy = np.zeros(4)
+        
+    # Compute the histogram of the axis
+    hist, bin_edges = np.histogram(window, bins=10, density=True)
+        
+    # Compute the entropy of the histogram
+    prob = hist[hist != 0]
+    entropy = -np.sum(prob * np.log2(prob))
+    
+    return entropy
+
+def entropyMagnitude(window):
+    magnitude = np.linalg.norm(window, axis=1)
+    hist, bin_edges = np.histogram(magnitude, bins=10, density=True)
+    
+    # Compute the entropy of the magnitude histogram
+    prob = hist[hist != 0]
+    entropy = -np.sum(prob * np.log2(prob))
+
+    return entropy
+
+def extract_features(window):
+    """
+    Here is where you will extract your features from the data over 
+    the given window. We have given you an example of computing 
+    the mean and appending it to the feature vector.
+    
+    """
+
+    """
+    Statistical
+    These include the mean, variance and the rate of zero- or mean-crossings. The
+    minimum and maximum may be useful, as might the median
+    
+    FFT features
+    use rfft() to get Discrete Fourier Transform
+    
+    Entropy
+    Integrating acceleration
+    
+    Peak Features:
+    Sometimes the count or location of peaks or troughs in the accelerometer signal can be
+    an indicator of the type of activity being performed. This is basically what you did in
+    assignment A1 to detect steps. Use the peak count over each window as a feature. Or
+    try something like the average duration between peaks in a window.
+    """
+
+    
+    x = []
+    feature_names = []
+    win = np.array(window)
+
+    x.append(_compute_mean_features(win[:,0]))
+    feature_names.append("x_mean")
+
+    x.append(_compute_mean_features(win[:,1]))
+    feature_names.append("y_mean")
+
+    x.append(_compute_mean_features(win[:,2]))
+    feature_names.append("z_mean")
+
+    # TODO: call functions to compute other features. Append the features to x and the names of these features to feature_names
+
+    x.append(compute_std(win[:,0]))
+    feature_names.append("std_x")
+
+    x.append(compute_std(win[:,1]))
+    feature_names.append("std_y")
+
+    x.append(compute_std(win[:,2]))
+    feature_names.append("std_z")
+
+    x.append(rms(win[:,0]))
+    feature_names.append("rms_x")
+
+    x.append(rms(win[:,1]))
+    feature_names.append("rms_y")
+
+    x.append(rms(win[:,2]))
+    feature_names.append("rms_z")
+
+    x.append(domFreq(win))
+    feature_names.append("dominant freq")
+
+    x.append(entropy(win[:,0]))
+    feature_names.append("entropy_x")
+
+    x.append(entropy(win[:,1]))
+    feature_names.append("entropy_y")
+
+    x.append(entropy(win[:,2]))
+    feature_names.append("entropy_z")
+
+    x.append(entropyMagnitude(win))
+    feature_names.append("entropy_mag")
+
+    feature_vector = list(x)
+    return feature_names, feature_vector
